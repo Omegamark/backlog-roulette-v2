50ca1c5e2e410c50a17a86b11c2a4d22e888b4e9
Mark Grant <grant.mark@gmail.com>
2018-09-27 16:05:36 -0600

    flapper concept working

---
+++ src/components/Flapper/flapper.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <hr />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
