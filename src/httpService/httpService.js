79946168824c076aa26a170abe9d59a18332c20f
Mike Dowd <mikedowd1972@gmail.com>
2018-09-27 18:11:42 -0600

    Added place for http calls, currently it returns a hardcoded object

---
+++ src/httpService/httpService.js
@@ -0,0 +1,56 @@
+const getGameInfo = () => [
+      {
+        label: "YODA",
+        imgUrl: "https://vignette.wikia.nocookie.net/starwars/images/d/df/Yoda%27s_hover_chair.png/revision/latest/scale-to-width-down/426?cb=20130810203813",
+      },
+      {
+        label: "LUKE",
+        imgUrl: "https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/59f094adf14aa1790f6c6bc6/1508938934244/new-luke-skywalker-image-from-the-last-jedi-and-director-rian-johnson-talks-about-lukes-exile4",
+      },
+      {
+        label: "LEAH",
+        imgUrl: "https://i.kinja-img.com/gawker-media/image/upload/s--c-ZfGdaN--/c_scale,f_auto,fl_progressive,q_80,w_800/qsjzp4ryybdx6uo4t4aw.png",
+      },
+      {
+        label: "DARTH",
+        imgUrl: "https://assets.geekinsider.com/wp-content/uploads/2014/02/darth-vader-original_0-696x522.jpg",
+      },
+      {
+        label: "HAN SOLO",
+        imgUrl: "https://cdn.theatlantic.com/assets/media/img/mt/2016/01/solo/lead_720_405.jpg?mod=1533691797",
+      },
+      {
+        label: "CHEWY",
+        imgUrl: "https://starwarsblog.starwars.com/wp-content/uploads/2015/06/chewie-db_2c0efea2-1536x864-284872943628.jpeg",
+      },
+      {
+        label: "OBI WAN",
+        imgUrl: "https://upload.wikimedia.org/wikipedia/en/3/32/Ben_Kenobi.png",
+      },
+      {
+        label: "R2D2",
+        imgUrl: "https://www.sideshowtoy.com/wp-content/uploads/2018/07/star-wars-r2-d2-legendary-scale-figure-sideshow-feature-400155.jpg",
+      },
+      {
+        label: "C3PO",
+        imgUrl: "https://lumiere-a.akamaihd.net/v1/images/C-3PO-See-Threepio_68fe125c.jpeg?region=0%2C1%2C1408%2C792&width=768",
+      },
+      {
+        label: "LANDO",
+        imgUrl: "https://cdn-images-1.medium.com/max/800/1*LmkKtcC4m1L8qU95i0TNSg.jpeg",
+      },
+      {
+        label: "Jabba",
+        imgUrl: "https://lumiere-a.akamaihd.net/v1/images/Jabba-The-Hutt_b5a08a70.jpeg?region=0%2C0%2C1200%2C675&width=768",
+      },
+      {
+        label: "Greedo",
+        imgUrl: "https://vignette.wikia.nocookie.net/starwars/images/c/c6/Greedo.jpg/revision/latest/scale-to-width-down/500?cb=20180209034210",
+      },
+      {
+        label: "Boba Fett",
+        imgUrl: "https://www.sideshowtoy.com/wp-content/uploads/2018/01/star-wars-boba-fett-sixth-scale-figure-hot-toys-feature-903351.jpg",
+      },
+    ];
+
+export { getGameInfo };
